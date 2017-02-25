24987c93ce756314c5542fd9e716a2834b7c29bd
Elliott-Greaves <greaveselliott@me.com>
2017-02-25 20:43:10 +0000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import jQuery from 'jquery';
+import CommentBox from './components/comment-box';
+
+
+jQuery(function() {
+  ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('comment-box'),
+    function() {
+      console.timeEnd('react-compile');
+    }
+  );
+})
