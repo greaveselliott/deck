815de18e57abc15c0b866bb53a70cd3fd4103f1e
Elliott-Greaves <greaveselliott@me.com>
2017-03-08 20:59:28 +0000

    Seperated navigation form layout to its own component

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Navigation from '../components/navigation';
 
 export default class Layout extends React.Component {
   render() {
     return (
       <div>
-        <div className="top-menu">
-          <ul>
-            <li>
-              <Link to="/decks">Decks</Link>
-            </li>
-            <li>
-              <Link to="/cards">Pictures</Link>
-            </li>
-            <li>
-              <Link to="/people">People</Link>
-            </li>
-            <li>
-              <Link to="/Settings">Settings</Link>
-            </li>
-          </ul>
-        </div>
+        <nav className="">
+          <Navigation/>
+        </nav>
 
         {this.props.children}
       </div>
