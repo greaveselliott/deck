0d01b256687278b105d6e0b8d30aaf6e628bedcf
Elliott-Greaves <greaveselliott@me.com>
2017-03-25 21:44:19 +0000

    began adding search icon SVG

---
+++ src/components/01_atoms/icon-search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class IconCardText extends React.Component {
+
+    render() {
+        let style = {
+            card:       {fill: '#e7eced'},
+            card__text: {fill: '#afb6bb'}
+        };
+
+        return (
+  
+        )
+    };
+};
